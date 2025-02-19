ec6023b04fee5cd951be5d8dcf879fd1ac903e79
Nathan A. <ndaabrantes@gmail.com>
2025-02-19 19:19:48 +0000

    Notes from class

---
+++ main.py
@@ -0,0 +1,41 @@
+print("Hello World!")
+
+my_variable = 6
+num_cats = 8
+
+print(num_cats)
+print("num_cats")
+
+print("num_cats: ", num_cats)
+print(f"number of cats: {num_cats}")
+print(f"My age: {my_variable} and my cats: {num_cats}")
+
+answer = input("What is your name?")
+print(f"You answered: {answer}")
+
+#artihmatic operators
+x = 6
+y = 20
+print(f"x = {x}")
+c = x + y
+print(c)
+
+print(f"c = {x+y}")
+print(f"x+y = {x*y}")
+print(f"x+y = {x/y}")
+print(f"x+y = {x**y}")
+
+# this is a comment
+# I can take lots of notes here, a pound is a note
+
+"""
+This is a multi-line comment
+anything in here will not be run
+"""
+
+answer1 = input("Give me a number! ")
+answer2 = input("Give me another number! ")
+num_1 = answer1
+num_2 = answer2
+sum = int(num_1) + int(num_2)
+print (f"{num_1} + {num_2} = {sum}")
